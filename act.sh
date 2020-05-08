a6da52ee147f17660bf013f12ce7d5fe3886f513
Leo Herrmann <leo.herrmann@physik.tu-berlin.de>
2020-05-08 08:34:59 +0200

    Add bash scripts

---
+++ act.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+PATH=$PATH:/data/data/com.termux/files/usr/bin/
+
+
+cd /storage/emulated/0/Dateien/plunderlist
+
+# ping -c 2 -W 1 -w 2 denkaktiv.de
---
+++ git.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+PATH=$PATH:/data/data/com.termux/files/usr/bin/
+
+cd /storage/emulated/0/Dateien/plunderlist
+
+git pull 2>>git.err
